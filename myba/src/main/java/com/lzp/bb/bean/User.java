49575ff505b69e6906ff170b07baeacca736cdd9
neo <2576518617@qq.com>
2021-04-24 11:08:05 +0800

    feat: lzp

---
+++ myba/src/main/java/com/lzp/bb/bean/User.java
@@ -0,0 +1,13 @@
+package com.lzp.bb.bean;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private  Date birth;
+
+}
